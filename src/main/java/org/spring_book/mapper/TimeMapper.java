7483c766a428e887fab4a1393fa1200c6aac3bb6
homill <homill@192.168.0.47>
2020-10-12 17:30:06 +0900

    2020-10-12/HoMill/TimeMapperTests.java

--- src/main/java/org/spring_book/mapper/TimeMapper.java
+++ src/main/java/org/spring_book/mapper/TimeMapper.java
@@ -1,5 +1,9 @@
 package org.spring_book.mapper;
 
+import org.apache.ibatis.annotations.Select;
+
 public interface TimeMapper {
 	
+	@Select("SELECT sysdate From dual")
+	public String getTime();
 }
